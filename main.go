77b957bdf90e2de2f97e6c21e6378c86cf377143
todd-estes <elestes@gmail.com>
2023-07-18 19:43:02 -0400

    Create new REPL functionality

--- main.go
+++ main.go
@@ -1,9 +1,94 @@
 package main
 
 import (
-	"fmt"
+    "bufio"
+    "fmt"
+    "os"
+    // "strings"
 )
 
+
+
+// return map[string]cliCommand{
+//     "help": {
+//         name:        "help",
+//         description: "Displays a help message",
+//         callback:    commandHelp,
+//     },
+//     "exit": {
+//         name:        "exit",
+//         description: "Exit the Pokedex",
+//         callback:    commandExit,
+//     },
+// }
+
+
+// StringPrompt asks for a string value using the label
+// func StringPrompt(label string) string {
+//     var s string
+//     r := bufio.NewReader(os.Stdin)
+//     for {
+//         fmt.Fprint(os.Stderr, label+" ")
+//         s, _ = r.ReadString('\n')
+//         if s != "" {
+//             break
+//         }
+//     }
+//     return strings.TrimSpace(s)
+// }
+
 func main() {
-	fmt.Println("hello world!")
+	reader := bufio.NewScanner(os.Stdin)
+		for {
+			fmt.Print("Pokedex > ")
+			reader.Scan()
+			input := reader.Text()
+
+		command, ok := getCommands()[input]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Printf("%s\n%s\n\n", "Welcome to the Pokedex!", "Usage:")
+
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
 }
