8c7de95a9058b9bad6484815ca549431c19516c9
todd-estes <elestes@gmail.com>
2023-07-20 16:06:36 -0400

    Update pokeapi with caching logic

--- pokeapi.go
+++ pokeapi.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"errors"
-	"io/ioutil"
+	"io"
 	"encoding/json"
+	"github.com/Todd-Estes/internal/pokecache"
 )
 
 type Locations struct {
@@ -20,44 +21,89 @@ type Locations struct {
 
 
 
-func getPokeLocations(loc *Locations) (string, error) {
-	res, err := http.Get(loc.Next)
+func getPokeLocations(url string, loc *Locations, c *pokecache.Cache) error {
+	_,ok := c.Cache[url]
+	if ok {
+	results := c.Cache[url].Val
+		outputCachedResults(loc, results)
+		return nil
+	}
+
+
+	// Get response from API
+	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
-		return "", errors.New("empty name")
+		return errors.New("empty name")
 	} 
 	defer res.Body.Close()
-	body, erruh := ioutil.ReadAll(res.Body)
+	body, erruh := io.ReadAll(res.Body)
 	if erruh != nil {
 		fmt.Println(err)
-		return "", errors.New("erruh error")
+		return errors.New("erruh error")
 	}
-	errah := json.Unmarshal(body, &loc)
-	if errah != nil {
-		fmt.Println(errah)
-	}
-	for _, result := range loc.Results {
-		fmt.Printf("%s\n", result.Name)
-	}
-	return "Success!!", nil
+
+	// Add URL and response to cache
+	c.Add(url, body)
+	// Maybe we can use outputCachedResults
+	outputCachedResults(loc, c.Cache[url].Val)
+
+	//Original code...lets keep this, but we're gonna comment it out for now
+
+
+
+	// errah := json.Unmarshal(body, &loc)
+	// if errah != nil {
+	// 	fmt.Println(errah)
+	// }
+	// for _, result := range loc.Results {
+	// 	fmt.Printf("%s\n", result.Name)
+	// }
+	// return "Success!!", nil
+	return nil
 }
 
-func getPrevPokeLocations(loc *Locations) (string, error) {
-	res, err := http.Get(loc.Previous)
-
-	if err != nil {
-		fmt.Println(err)
-		return "", errors.New("empty name")
-	} 
-	defer res.Body.Close()
-	body, erruh := ioutil.ReadAll(res.Body)
-	if erruh != nil {
-		fmt.Println(err)
-		return "", errors.New("erruh error")
+func getPrevPokeLocations(loc *Locations, c *pokecache.Cache) error {
+		url := loc.Previous
+		_, ok := c.Cache[url]
+	if ok {
+	results := c.Cache[url].Val
+		outputCachedResults(loc, results)
+		return nil
+	} else {
+		getPokeLocations(url, loc, c)
+		return nil
 	}
-	errah := json.Unmarshal(body, &loc)
-	if errah != nil {
-		fmt.Println(errah)
+
+
+
+	// res, err := http.Get(loc.Previous)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return "", errors.New("empty name")
+	// } 
+	// defer res.Body.Close()
+	// body, erruh := io.ReadAll(res.Body)
+	// if erruh != nil {
+	// 	fmt.Println(err)
+	// 	return "", errors.New("erruh error")
+	// }
+	// errah := json.Unmarshal(body, &loc)
+	// if errah != nil {
+	// 	fmt.Println(errah)
+	// }
+	// for _, result := range loc.Results {
+	// 	fmt.Printf("%s\n", result.Name)
+	// }
+	// return "Success!!", nil
+}
+
+func outputCachedResults(loc *Locations, results []byte) (string, error) {
+	error := json.Unmarshal(results, loc)
+	if error != nil {
+		fmt.Println(error)
+		return "", error
 	}
 	for _, result := range loc.Results {
 		fmt.Printf("%s\n", result.Name)
